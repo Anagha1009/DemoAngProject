e9f27274452bfe91fd2102bfae29731fa4618710
Anagha Thorat <t.anagha8@gmail.com>
2022-01-04 11:03:24 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserComponent} from '../app/user/user.component';
+import {AdminComponent} from '../app/admin/admin.component';
+import{PageNotFoundComponent} from '../app/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'admin',
+    component:AdminComponent
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import{StudentModule} from './student/student.module';
 import{FormsModule} from '@angular/forms';
+import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AdminComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
